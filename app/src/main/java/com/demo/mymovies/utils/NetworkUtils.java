ef491660158323b4ffb963c3682114c3e6329250
WhiteCatDeveloper <whitecatsdeveloper@gmail.com>
2021-03-04 17:37:48 +0400

    Create project

---
+++ app/src/main/java/com/demo/mymovies/utils/NetworkUtils.java
@@ -0,0 +1,7 @@
+package com.demo.mymovies.utils;
+
+public class NetworkUtils {
+
+    private static final String API_KEY = APIKey.getApiKey();
+    private static final String BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+}
